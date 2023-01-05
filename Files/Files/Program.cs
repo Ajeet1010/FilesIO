07b1bfd1ae67b3b48c1dee04a1e7a225b386d9d6
ajeet1010 <ajeetpal101999@gmail.com>
2023-01-06 00:52:30 +0530

    [Ajeet1010]Add: Checking file exists

---
+++ Files/Files/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Visual Studio\.Net\FilesIO\Files\Files\TextFile1.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists ");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+        }
+    }
+}
