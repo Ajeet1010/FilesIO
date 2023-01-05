f6944aba13695f66765b3f11763e095361a811b4
ajeet1010 <ajeetpal101999@gmail.com>
2023-01-06 01:44:59 +0530

    [Ajeet1010]Add: Reading all text of text file

--- Files/Files/Program.cs
+++ Files/Files/Program.cs
@@ -12,14 +12,10 @@ namespace Files
         {
             string path = @"E:\Visual Studio\.Net\FilesIO\Files\Files\TextFile1.txt";
             
-            // Read All Lines
-            string[] lines;
-            lines = File.ReadAllLines(path);
-            Console.WriteLine(lines[0]);
-            Console.WriteLine(lines[1]);
-            Console.WriteLine(lines[2]);
-            Console.WriteLine(lines[3]);
-            Console.WriteLine(lines[4]);
+            // Read All Text
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
         }
     }
 }
